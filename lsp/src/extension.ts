1071e2d93d8545550317ea20eb25d597c53115d9
Julian Hofmann <julian.hofmann2805@gmail.com>
2022-09-26 23:43:56 +0200

    Fix embe-ls not starting when installed in ~/.local/bin

--- lsp/src/extension.ts
+++ lsp/src/extension.ts
@@ -6,19 +6,27 @@ import {
 	LanguageClientOptions,
 	TransportKind
 } from 'vscode-languageclient/node';
+import { openStdin } from 'process';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
 	const serverOptions: Executable = {
-		command: "embe-ls",
-		transport: TransportKind.stdio,
-		args: []
+		command: "embe-ls"
 	};
 
 	const clientOptions: LanguageClientOptions = {
 		documentSelector: [{ scheme: 'file', language: 'embe' }]
 	};
+
+	if (process.platform != "win32") {
+		if (!process.env["PATH"].includes("/usr/local/bin")) {
+			process.env["PATH"] += ":/usr/local/bin";
+		}
+		if (!process.env["PATH"].includes(process.env["HOME"] + "/.local/bin")) {
+			process.env["PATH"] += ":" + process.env["HOME"] + "/.local/bin";
+		}
+	}
 
 	client = new LanguageClient(
 		'embe-ls',
